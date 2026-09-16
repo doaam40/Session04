47ce6bdf07bd443a764614d74f69292d088e936f
_Doaa_ <_Doaa_@DOAA>
2026-09-16 16:26:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session04_AssignmentC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
