8a6fe77d996d7219b642749b87b9ee960e31a2b8
_Doaa_ <_Doaa_@DOAA>
2026-09-16 16:40:10 +0300

    q2

--- Program.cs
+++ Program.cs
@@ -5,8 +5,16 @@
         static void Main(string[] args)
         {
             #region Q1
-            double[] prices = { 25.5, 40.0, 33.75 };
-            Console.WriteLine($"price[1]: {prices[1]}");
+            //double[] prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine($"price[1]: {prices[1]}");
+            #endregion
+
+            #region Q2
+            int[,] shelfCopies = {
+                { 3, 5 },
+                { 1, 4 }
+            };
+            Console.WriteLine($"shelfCopies[1, 0]: {shelfCopies[1,0]}");
             #endregion
         }
     }
